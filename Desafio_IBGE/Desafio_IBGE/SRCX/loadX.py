e7c029d7747a80fd02f12bf48ada891233f0b91c
Matheus Lucena <63615329+MatheusSLucena@users.noreply.github.com>
2026-08-19 20:46:12 -0300

    Delete Desafio_IBGE directory
    Deletando primeira versão

--- Desafio_IBGE/Desafio_IBGE/SRCX/loadX.py
+++
@@ -1,8 +0,0 @@
-import json
-
-class LoadX():
-    def __initX__(self):
-        pass
-    def load_json(self, nome_doc, data):
-        with open(f"{nome_doc}.json","w",encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False,indent=2)
